e1eee3db0c919e5e2d763c08d9b38e3e8d1ebb65
AlissonRC <alissonrdacruz@gmail.com>
2020-09-13 10:53:34 -0300

    added node-fetch in the server side, removed random logic to the server side and added it to the client side

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-var porta = process.env.PORT || 8080;
+var porta = process.env.PORT || 9090;
 const api_url =
   "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=0491B65D014FAA237EA9C65D1387304D";
 
-const request = require("request");
+const fetch = require("node-fetch");
 const express = require("express");
-const cors = require("cors");
 
 const api = express();
-api.use(cors());
+api.use(express.static("."));
 api.listen(porta, () => {
   console.log("running");
 });
 
-api.get("/heroes/random", (req, res) => {
-  request.get(api_url, (error, response, body) => {
-    if (error) {
-      return console.log(error);
-    }
-    const body_resp = JSON.parse(body);
-    const result = body_resp.result;
-    console.log(result);
-    const arrayId = [];
-
-    result.heroes.forEach((element) => {
-      arrayId.push(element.id);
-    });
-    let randomizer = Math.floor(Math.random() * (arrayId.length - 1 - 0) + 0);
-    const randomID = arrayId[randomizer];
-    const hero = result.heroes.filter((element, id) => {
-      return element.id === randomID;
-    });
-    console.log(arrayId);
-    console.log(hero);
-    res.send(hero);
-  });
+api.get("/heroes/random", async (req, res) => {
+  try {
+    const fetch_response = await fetch(api_url);
+    const data = await fetch_response.json();
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    console.log("error: ", error);
+  }
 });
