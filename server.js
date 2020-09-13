b7d74d297fcae20cc91443502cdda57efabbfc25
AlissonRC <alissonrdacruz@gmail.com>
2020-09-13 16:34:04 -0300

    env in the api key

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var porta = process.env.PORT || 9090;
-const api_url =
-  "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=0491B65D014FAA237EA9C65D1387304D";
 
 const fetch = require("node-fetch");
 const express = require("express");
+require("dotenv").config();
 
 const api = express();
 api.use(express.static("."));
 api.listen(porta, () => {
   console.log("running");
 });
+const api_key = process.env.API_KEY;
+const api_url = `https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=${api_key}`;
 
 api.get("/heroes/random", async (req, res) => {
   try {
